d2a642dacd6f1c9036003f4060584fd56d612651
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-10 00:45:46 -0300

    chore: configuracion inicial con la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // Configuracion global de variables de entorno
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // Configuracion de base de datos, es asincronica porque necesitamos inyectar el ConfigService antes de conectar a la base de datos
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true, // En produccion, es mejor usar migraciones en lugar de synchronize
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
