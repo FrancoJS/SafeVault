01976fe18d5fe78bd5586e9eb45cedbc5588efab
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-11 12:36:22 -0300

    chore: update environment variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,12 +14,13 @@ import { SecretsModule } from './secrets/secrets.module';
     }),
     // Configuracion de base de datos, es asincronica porque necesitamos inyectar el ConfigService antes de conectar a la base de datos
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get<string>('DB_HOST'),
         port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
+        username: configService.get<string>('DB_USER'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_NAME'),
         autoLoadEntities: true,
