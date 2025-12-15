3ecb4ba94583066a128d83492277d02cb8cde255
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-15 17:10:21 -0300

    add validation rules to CreateSecretDto

--- src/secrets/dto/create-secret.dto.ts
+++ src/secrets/dto/create-secret.dto.ts
@@ -1,4 +1,12 @@
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateSecretDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre no puede estar vacío' })
   name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El valor no puede estar vacío' })
+  @MinLength(6, { message: 'El valor debe tener al menos 6 caracteres' })
   value: string;
 }
