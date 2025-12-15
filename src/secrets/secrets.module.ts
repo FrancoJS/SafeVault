eac12d976c9f62cb1d4bef324df12349d11d1c89
Franco_SJ <jean.jara.fs@gmail.com>
2025-12-15 15:01:17 -0300

    feat(secret): create secret service logic

--- src/secrets/secrets.module.ts
+++ src/secrets/secrets.module.ts
@@ -3,9 +3,10 @@ import { SecretsService } from './secrets.service';
 import { SecretsController } from './secrets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Secret } from './entities/secret.entity';
+import { EncryptionModule } from 'src/encryption/encryption.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Secret])],
+  imports: [TypeOrmModule.forFeature([Secret]), EncryptionModule],
   controllers: [SecretsController],
   providers: [SecretsService],
 })
--- src/secrets/secrets.service.ts
+++ src/secrets/secrets.service.ts
@@ -1,26 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSecretDto } from './dto/create-secret.dto';
-import { UpdateSecretDto } from './dto/update-secret.dto';
+// import { UpdateSecretDto } from './dto/update-secret.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Secret } from './entities/secret.entity';
+import { Repository } from 'typeorm/repository/Repository';
+import { EncryptionService } from 'src/encryption/encryption.service';
 
 @Injectable()
 export class SecretsService {
-  create(createSecretDto: CreateSecretDto) {
-    return 'This action adds a new secret';
+  constructor(
+    @InjectRepository(Secret)
+    private readonly secretsRepository: Repository<Secret>,
+    private readonly encryptionService: EncryptionService,
+  ) {}
+
+  async create(createSecretDto: CreateSecretDto) {
+    const { name, value } = createSecretDto;
+
+    const { encryptedData, iv } = this.encryptionService.encrypt(value);
+
+    const newSecret = this.secretsRepository.create({
+      name,
+      encryptedValue: encryptedData,
+      iv,
+    });
+
+    return await this.secretsRepository.save(newSecret);
   }
 
-  findAll() {
-    return `This action returns all secrets`;
+  async findAll() {
+    return await this.secretsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} secret`;
+  async findOne(id: string) {
+    const secret = await this.secretsRepository.findOneBy({ id });
+
+    if (!secret) {
+      throw new NotFoundException(`Secreto con el ID ${id} no encontrado`);
+    }
+
+    const decryptedValue = this.encryptionService.decrypt(secret.encryptedValue, secret.iv);
+
+    return { ...secret, value: decryptedValue };
   }
 
-  update(id: number, updateSecretDto: UpdateSecretDto) {
-    return `This action updates a #${id} secret`;
-  }
+  // update(id: number, updateSecretDto: UpdateSecretDto) {
+  //   return `This action updates a #${id} secret`;
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} secret`;
-  }
+  // remove(id: number) {
+  //   return `This action removes a #${id} secret`;
+  // }
 }
